d92f39c8ebd5ca2742076b82216d4a4833983738
Luka <lukagoderdzishvili4@gmail.com>
2023-11-19 17:30:44 +0400

    [1.Task] Added init and manifest files

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Hospital Management',
+    'version':'1.0.0',
+    'sequence': -100,
+    'depends': [],
+    'category': 'Hospital',
+    'description': """Hospital management system""",
+    'data':[],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
