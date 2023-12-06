ff623e20862484d41c3f6e3f4f4554430758f183
Luka <lukagoderdzishvili4@gmail.com>
2023-12-06 18:46:13 +0400

    [21.Task] Added date and datetime fields in appointment

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -8,3 +8,6 @@ class HospitalAppointment(models.Model):
 
     name = fields.Char()
     patient_id = fields.Many2one("hospital.patient", string="Patient")
+    appointment_time = fields.Datetime(string="Appointment Time")
+    booking_date = fields.Date(string="Booking Date")
+    
