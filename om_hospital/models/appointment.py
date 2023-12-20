6eabffe3e731cccab00d9dd42605d4ce6ae8dadd
Luka <lukagoderdzishvili4@gmail.com>
2023-12-20 19:07:08 +0400

    [22.Task] set deafult values for date and datetime fields

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -8,6 +8,6 @@ class HospitalAppointment(models.Model):
 
     name = fields.Char()
     patient_id = fields.Many2one("hospital.patient", string="Patient")
-    appointment_time = fields.Datetime(string="Appointment Time")
-    booking_date = fields.Date(string="Booking Date")
+    appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
+    booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
     
