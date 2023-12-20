ddadb3f762c339687933fb8be6c1f08a61e48372
Luka <lukagoderdzishvili4@gmail.com>
2023-12-20 19:22:57 +0400

    [23.Task] Added related field

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -8,6 +8,7 @@ class HospitalAppointment(models.Model):
 
     name = fields.Char()
     patient_id = fields.Many2one("hospital.patient", string="Patient")
+    gender = fields.Selection(string='Gender', related="patient_id.gender")
     appointment_time = fields.Datetime(string="Appointment Time", default=fields.Datetime.now)
     booking_date = fields.Date(string="Booking Date", default=fields.Date.context_today)
     
