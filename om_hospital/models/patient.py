e9bfad44fed055c8a4f29d920ce276692f272ac5
Luka <lukagoderdzishvili4@gmail.com>
2023-12-22 13:36:30 +0400

    [24.Task] Compute age

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -1,4 +1,6 @@
+from datetime import date
 from odoo import api, fields, models
+
 
 
 class HospitalPatient(models.Model):
@@ -7,7 +9,18 @@ class HospitalPatient(models.Model):
     _description = 'Hospital Patient'
 
     name = fields.Char(string='Name', tracking=True)
+    date_of_birth = fields.Date(string="Date of Birth")
     reference = fields.Char(string="Reference")
-    age = fields.Integer(string='Age', tracking=True)
+    age = fields.Integer(string='Age', compute="_compute_age", tracking=True, store=True)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True)
     active = fields.Boolean(default=True)
+
+    @api.depends("date_of_birth")
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year - ((today.month, today.day) < (rec.date_of_birth.month, rec.date_of_birth.day))
+            else:
+                rec.age = 0
+                
