26953a8fbf55bc94a0f97ff587fa05c23d7fcee0
Luka <lukagoderdzishvili4@gmail.com>
2024-01-08 18:29:00 +0400

    [68.Task] Added new module with inherited model

---
+++ om_odoo_inheritance/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'om_odoo_inheritance',
+    'version':'1.0.0',
+    'sequence': -100,
+    'depends': ['sale'],
+    'category': 'om odoo inheritance',
+    'description': """""",
+    'data':[
+    ],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
