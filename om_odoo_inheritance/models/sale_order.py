7e8ce0bc5931ee39824f44e9bd72e4fea504c4aa
Luka <lukagoderdzishvili4@gmail.com>
2024-01-08 19:14:24 +0400

    [70.Task] Added inherit method with logic and added gitignore file

--- om_odoo_inheritance/models/sale_order.py
+++ om_odoo_inheritance/models/sale_order.py
@@ -4,4 +4,9 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    confirmed_user_id = fields.Many2one("res.users")
+    confirmed_user_id = fields.Many2one("res.users", readonly=True, help="This field will be set with the current user when the Confirm button is clicked")
+
+    def action_confirm(self):
+        super(SaleOrder, self).action_confirm()
+        self.confirmed_user_id = self.env.user.id
+        
