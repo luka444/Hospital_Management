d9ca96bfe85ba19b6ac95613b48ea17b03eaaba4
Luka <lukagoderdzishvili4@gmail.com>
2024-01-08 17:01:21 +0400

    [61.Task] Added transientmodel

---
+++ om_hospital/wizard/cancel_appointment_wizard.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+
+class CancelAppointmentWizard(models.TransientModel):
+    _name = "cancel.appointment.wizard"
+    _description = "Cancel Appointment Wizard"
+
+    appointment_id = fields.Many2one("hospital.appointment", string="Appointment")
