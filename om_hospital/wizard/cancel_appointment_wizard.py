de44bab598d210dd66ff3cb3ca545e6bc67577d2
Luka <lukagoderdzishvili4@gmail.com>
2024-01-09 17:35:21 +0400

    [74.Task] Added default get method

--- om_hospital/wizard/cancel_appointment_wizard.py
+++ om_hospital/wizard/cancel_appointment_wizard.py
@@ -1,12 +1,21 @@
-from odoo import fields, models
+import datetime
+from odoo import fields, models, api
 
 
 class CancelAppointmentWizard(models.TransientModel):
     _name = "cancel.appointment.wizard"
     _description = "Cancel Appointment Wizard"
 
+    @api.model
+    def default_get(self, fields):
+        res = super(CancelAppointmentWizard, self).default_get(fields)
+        res["date_cancel"] = datetime.date.today()
+        return res
+
     appointment_id = fields.Many2one("hospital.appointment", string="Appointment")
     reason = fields.Text(string="Reason")
+    date_cancel = fields.Date(string="Cancel Date")
 
     def action_cancel(self):
         return
+        
