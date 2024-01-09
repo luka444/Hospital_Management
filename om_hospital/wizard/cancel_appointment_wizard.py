15121dfd1e8e54db2f1b85f07532e85a92c3b556
Luka <lukagoderdzishvili4@gmail.com>
2024-01-09 19:09:50 +0400

    [78.Task] added default value in wizard using by active id

--- om_hospital/wizard/cancel_appointment_wizard.py
+++ om_hospital/wizard/cancel_appointment_wizard.py
@@ -10,6 +10,7 @@ class CancelAppointmentWizard(models.TransientModel):
     def default_get(self, fields):
         res = super(CancelAppointmentWizard, self).default_get(fields)
         res["date_cancel"] = datetime.date.today()
+        res["appointment_id"] = self.env.context.get('active_id')
         return res
 
     appointment_id = fields.Many2one("hospital.appointment", string="Appointment")
